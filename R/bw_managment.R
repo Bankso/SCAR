f094802d7c67825a151506f03ab745e71ea1a7ca
Bankso <49208907+Bankso@users.noreply.github.com>
2021-11-08 17:50:09 -0800

    Ref change that was throwing error

--- R/bw_managment.R
+++ R/bw_managment.R
@@ -1,6 +1,6 @@
 
 #' Add bigwigs to Sample Sheet
-#'
+#' @import stringr
 #' @param SCAR_obj SCAR object.
 #' @param alignment_dir Directory of aligned reads.
 #' @param comp_op Operation for bamCompare
@@ -8,22 +8,22 @@
 
 add_bws <- function(
   SCAR_obj,
-  alignment_dir=alignment_dir,
-  comp_op=comp_op
+  alignment_dir,
+  comp_op = 'log2'
 ) {
 
   ## Grab some info from object and prepare inputs.
   analysis_type <- pull_setting(SCAR_obj, "analysis_type")
   compare <- as.logical(pull_setting(SCAR_obj, "compare"))
+  alignment_dir <- pull_setting(SCAR_obj, 'alignment_dir')
   
   sample_sheet <- copy(SCAR_obj@sample_sheet)
 
   if (compare == TRUE) {
-    sample_sheet[,
-      sample_bws := str_c(
-        alignment_dir, sample_name, "_", comp_op, "_control.bw")
-    ]
-  }
+    sample_sheet[, sample_bws := str_c(
+      alignment_dir, sample_name, "_", comp_op, "_control.bw")
+      ]
+    }
   
   else {
     sample_sheet[,
